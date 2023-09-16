ce3364710b342e8212630ec0d6b55d83d23ed7a9
raifran1 <raifranlucas@outlook.com>
2023-09-16 10:40:40 -0300

    feat: configurando sigla de moeda como unico para evitar cadastro repetidos

--- app/moeda/models.py
+++ app/moeda/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Coin(models.Model):
     name = models.CharField(verbose_name='Nome', max_length=150)
-    acronym = models.CharField(verbose_name='Sigla', max_length=4)
+    acronym = models.CharField(verbose_name='Sigla', max_length=4, unique=True)
 
     class Meta:
         verbose_name = 'Moeda'
