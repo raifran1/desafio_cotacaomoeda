9884ff7e45aa0536da0ac06d088a7c28efd96c10
raifran1 <raifranlucas@outlook.com>
2023-09-18 15:12:34 -0300

    feat: configurando pré carregamento do grafico

--- app/website/views.py
+++ app/website/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
+from cotacao_moeda.models import Quotation
+
 
 def home(request):
     """
-    View para renderizar a pagina principal da aplicação, retornando dados necessários para visualização e construção
-    dos gráficos.
+    View para renderizar a pagina principal da aplicação, a montagem dos gráficos é realizado via JS e API
     """
+    quotations = Quotation.objects.all()[:5]
     return render(request, 'website/home.html', locals())
