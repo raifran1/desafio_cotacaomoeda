47ccb527069a085605a261a4b1f4b7c91bf825f4
raifran1 <raifranlucas@outlook.com>
2023-09-21 08:20:47 -0300

    feat: criando os primeiros tests

--- app/moeda/tests.py
+++ app/moeda/tests.py
@@ -1,3 +1,16 @@
+import requests
 from django.test import TestCase
 
-# Create your tests here.
+from moeda.models import Coin
+
+
+class CoinTestCase(TestCase):
+
+
+    def test_name_coin_method_returns(self):
+        coin_obj = Coin(name="Dólar")
+        self.assertEqual(str(coin_obj), coin_obj.name)
+
+    def test_api_coin(self):
+        response = requests.get('https://cotaon.raifranlucas.dev/api/coins/')
+        assert response.status_code == 200
